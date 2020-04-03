4650eaa5de36389d3bb0078aa657634233a38e33
augustocolo <augusto.colongo@studenti.polito.it>
2020-04-03 17:15:15 +0200

    Creata UI

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -1,23 +1,93 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ChoiceBox<?> DDCorsi;
 
+    @FXML
+    private Button btnCercaIscritti;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private Button btnCercaMatricola;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private Button btnCercaCorsi;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private TextArea areaTxt;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaIscritti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaMatricola(ActionEvent event) {
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {    	
+        assert DDCorsi != null : "fx:id=\"DDCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaIscritti != null : "fx:id=\"btnCercaIscritti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaMatricola != null : "fx:id=\"btnCercaMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert areaTxt != null : "fx:id=\"areaTxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        btnCercaMatricola.setStyle("-fx-background-color: #2B580C;");
     }
 }
